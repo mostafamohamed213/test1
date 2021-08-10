31c886c5a97b792b2a0bcf182245d5c310bb7eb8
mostafamohamed213 <64440025+mostafamohamed213@users.noreply.github.com>
2021-08-10 14:18:40 +0200

    homece

--- WebApplication3/WebApplication3/Controllers/HomeController.cs
+++ WebApplication3/WebApplication3/Controllers/HomeController.cs
@@ -18,15 +18,16 @@ namespace WebApplication3.Controllers
             _logger = logger;
         }
 
+        public IActionResult howt()
+        {
+            return View();
+        }
+        
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
